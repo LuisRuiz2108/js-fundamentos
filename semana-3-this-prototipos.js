38a6cb681e6ca75309c9bbfe01bf281e47db2238
Luis Ruiz <luis.21.ruiz.08@gmail.com>
2026-07-28 18:50:07 -0600

    Uncomment examples

--- semana-3-this-prototipos.js
+++ semana-3-this-prototipos.js
@@ -9,47 +9,47 @@ const otraPersona = {
     nombre: 'Nahomi',
 }
 
-// persona.saludar(); // Hola, soy Luis
+persona.saludar(); // Hola, soy Luis
 
-// const saludarSuelto = persona.saludar;
-// saludarSuelto(); // Hola, soy undefined
+const saludarSuelto = persona.saludar;
+saludarSuelto(); // Hola, soy undefined
 
-// const saludarSuelto = persona.saludar.bind(persona);
-// saludarSuelto(); // Hola, soy Luis
+const saludarSuelto = persona.saludar.bind(persona);
+saludarSuelto(); // Hola, soy Luis
 
-// persona.saludar.call(persona); // Hola, soy Luis
-// const saludarSuelto = persona.saludar; // Hola, soy Luis
-// saludarSuelto();
+persona.saludar.call(persona); // Hola, soy Luis
+const saludarSuelto = persona.saludar; // Hola, soy Luis
+saludarSuelto();
 
-// const contador = {
-//   cuenta: 0,
-//   iniciar: function () {
-//     setTimeout( ()  => {
-//       this.cuenta = this.cuenta + 1;
-//       console.log(this.cuenta);
-//     }, 100);
-//   },
-// };
+const contador = {
+  cuenta: 0,
+  iniciar: function () {
+    setTimeout( ()  => {
+      this.cuenta = this.cuenta + 1;
+      console.log(this.cuenta);
+    }, 100);
+  },
+};
 
-// contador.iniciar();
+contador.iniciar();
 
-// function Persona(nombre) {
-//   this.nombre = nombre;
-// }
+function Persona(nombre) {
+  this.nombre = nombre;
+}
 
-// // Persona.prototype.saludar = function () {
-// //   console.log("Hola, soy " + this.nombre);
-// // };
+Persona.prototype.saludar = function () {
+  console.log("Hola, soy " + this.nombre);
+};
 
-// const luis = new Persona("Luis");
-// const nomi = new Persona("Nomi");
+const luis = new Persona("Luis");
+const nomi = new Persona("Nomi");
 
-// console.log(Persona.saludar)
-// console.log(luis.hasOwnProperty('saludar'))
-// console.log(luis.__proto__ === Persona.prototype)
+console.log(Persona.saludar)
+console.log(luis.hasOwnProperty('saludar'))
+console.log(luis.__proto__ === Persona.prototype)
 
-// luis.saludar();
-// nomi.saludar();
+luis.saludar();
+nomi.saludar();
 
 function Animal(nombre) {
     this.nombre = nombre;
